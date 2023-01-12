4b323a15865b346447d6f77b954632b7b8f2ec6c
marta <martalbarbosa@gmail.com>
2023-01-12 09:27:02 +0000

    uploaded csv of Ml price

---
+++ streamlit_price_ML.py
@@ -0,0 +1,9 @@
+#import libraries
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.set_page_config(page_title="Data Readers_app", layout="wide", menu_items=None)
+st.title('Hello')
+
